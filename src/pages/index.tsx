3b5292c68a21185b3f1eb01c9aa45af04f0dbb12
Marcock2000 <92387883+Marcock2000@users.noreply.github.com>
2023-03-28 15:22:33 +0300

    ts check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type{ NextPage } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
-import {  motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Home: NextPage = () => {
   const [image, setImage] = useState<File | null>(null);
